1fc9e111e4e0a75be6e3545ce8ae7ae0be4478cd
blkdev02 <davidoffeiboadu@gmail.com>
2020-06-07 10:09:19 +0100

    changes to module 4 v2

--- jobs/app.py
+++ jobs/app.py
@@ -1,17 +1,17 @@
 import sqlite3
 from flask import Flask, render_template, g
 
-PATH=Users/david.boadu/Documents/my_skills_improvement/pluralsight_project/v2/PythonFlask-JobBoard/db/jobs.sqlite
+PATH='db/jobs.sqlite'
 
 app = Flask(__name__)
 
 def open_conection():
-    getattr(_connection = None)
+    getattr(g, _connection, None)
     connection = getattr
-    if connection == None:
+    if connection is None:
         connection = sqlite3.connect(PATH) 
         g._connection = sqlite3.connect(PATH) 
-    row_factory._connection = sqlite3.Row
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values, commit, single):
