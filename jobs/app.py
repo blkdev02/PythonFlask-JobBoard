dd323c54b3b97bc99d3a1d392ea197bd72ff6b46
blkdev02 <davidoffeiboadu@gmail.com>
2020-06-07 13:15:52 +0100

    changes to module 5 v8

--- jobs/app.py
+++ jobs/app.py
@@ -1,14 +1,14 @@
 import sqlite3
 from flask import Flask, render_template, g
 
-PATH='db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g,'_connection', None)
-    if connection is None:
-        connection = g._connection = sqlite3.connect(PATH) 
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -19,19 +19,18 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    if connection is not None: 
+    if connection is not None:
         connection.close()
 
-
-@app.route("/")
-@app.route("/jobs")
+@app.route('/')
+@app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
-    
